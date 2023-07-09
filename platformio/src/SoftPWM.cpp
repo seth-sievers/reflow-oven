7528a81491c29bdfbf73e23be61c8f0d35de4505
Seth <ses3wn@umsystem.edu>
2023-07-09 14:48:59 -0500

    file error

--- platformio/src/SoftPWM.cpp
+++ platformio/src/SoftPWM.cpp
@@ -1,5 +1,10 @@
 #include "SoftPWM.h"
 
 /* ------------------------- SOFT_PWM_IMPLEMENTATION ------------------------ */
-
+SoftPWM::SoftPWM(const unsigned short PIN, const unsigned short FREQ)
+{
+        pin = PIN; 
+        freq = FREQ; 
+        return; 
+}
 /* -------------------------------------------------------------------------- */
