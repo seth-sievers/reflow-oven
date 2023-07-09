c7f2ac0f4551687a427fcd706091b3dc03ba715b
Seth <ses3wn@umsystem.edu>
2023-07-09 11:44:31 -0500

    state 1 completed

--- platformio/src/main.cpp
+++ platformio/src/main.cpp
@@ -9,7 +9,7 @@
 #include "Stdint.h"
 
 /* ---------------------------- GLOBAL_VARIABLES ---------------------------- */
-//Temperature Vars
+// Temperature Vars
 MAX6675 TMP(10);
 MAX6675 TMP_UPPER(9); 
 MAX6675 TMP_LOWER(8); 
@@ -23,6 +23,12 @@ float TMP_C = 0;
 float TMP_UPPER_C = 0; 
 float TMP_LOWER_C = 0; 
 
+char BUF[8]; // Serial Receive Buffer
+
+// State machine
+short STATE = 0;
+unsigned long int LAST_READY_MS = 0;  
+
 
 unsigned long int test_MS = 0; 
 /* -------------------------------------------------------------------------- */
@@ -34,9 +40,15 @@ float sum(const float array[4]);
 /* ---------------------------------- SETUP --------------------------------- */
 void setup() 
 {
+        //setup serial and buffer
         Serial.begin(115200);
         Serial.setTimeout(250); 
-        pinMode(13, OUTPUT); 
+        for (short i = 0; i < 8; i++) {
+                BUF[i] = '\0'; 
+        }
+
+        pinMode(4, OUTPUT); 
+        digitalWrite(4, LOW);
 
 }
 /* -------------------------------------------------------------------------- */
@@ -57,15 +69,45 @@ void loop()
                 TMP_LOWER_C = sum(TMP_LOWER_BUF)/4.0;
         }
 
-        if ((millis() - test_MS) > 250){
-                Serial.print(F("TMP: "));
-                Serial.print(TMP_C);
-                Serial.print(F(" TMP_UPPER: "));
-                Serial.print(TMP_UPPER_C);
-                Serial.print(F(" TMP_LOWER: "));
-                Serial.println(TMP_LOWER_C);
-                test_MS = millis(); 
+        // State machine controlling operation
+        switch(STATE)
+        {
+                case 0:
+                        //send READY signal and check for response
+                        if ((millis() - LAST_READY_MS) > 100) {
+                                Serial.println(F("READY")); 
+                                LAST_READY_MS = millis(); 
+                        }
+                        if ((Serial.available() > 0)) {
+                                Serial.readBytesUntil('\n', BUF, 8);
+                                if (strcmp(BUF, "ACK") == 0) {
+                                        digitalWrite(4,HIGH); 
+                                        STATE = 1; 
+                                }
+                        }
+                        break; 
+                case 1:
+                        break; 
+                case 2:
+                        break; 
         }
+
+
+
+
+
+
+
+
+        // if ((millis() - test_MS) > 250){
+        //         Serial.print(F("TMP: "));
+        //         Serial.print(TMP_C);
+        //         Serial.print(F(" TMP_UPPER: "));
+        //         Serial.print(TMP_UPPER_C);
+        //         Serial.print(F(" TMP_LOWER: "));
+        //         Serial.println(TMP_LOWER_C);
+        //         test_MS = millis(); 
+        // }
 }
 /* -------------------------------------------------------------------------- */
 
