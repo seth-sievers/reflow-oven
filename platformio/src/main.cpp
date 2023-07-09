0fd6439d742d03fdb2ac7cfbe09008fbbc8a8739
Seth <ses3wn@umsystem.edu>
2023-07-09 11:24:06 -0500

    basic temperature monitoring implemented

--- platformio/src/main.cpp
+++ platformio/src/main.cpp
@@ -1,41 +1,82 @@
+//Author: Seth Sievers
+//Date: 7/9/23
+//Purpose: This Arduino Program will communicate with the python based host 
+//      controller script for the purpose of controlling a modified toaster oven
+//      reflow oven
+
 #include "Arduino.h"
+#include "MAX6675.h"
+#include "Stdint.h"
 
-char buf[8]; 
-bool received = false; 
+/* ---------------------------- GLOBAL_VARIABLES ---------------------------- */
+//Temperature Vars
+MAX6675 TMP(10);
+MAX6675 TMP_UPPER(9); 
+MAX6675 TMP_LOWER(8); 
+float TMP_BUF[4];
+float TMP_UPPER_BUF[4];
+float TMP_LOWER_BUF[4]; 
+unsigned long int LAST_SAMPLE_MS = 0; 
+unsigned long int NUM_SAMPLES = 0; 
+unsigned int BUF_INDEX = 0; 
+float TMP_C = 0; 
+float TMP_UPPER_C = 0; 
+float TMP_LOWER_C = 0; 
 
-void setup()
+
+unsigned long int test_MS = 0; 
+/* -------------------------------------------------------------------------- */
+
+/* ----------------------------- FUNCTION_STUBS ----------------------------- */
+float sum(const float array[4]); 
+/* -------------------------------------------------------------------------- */
+
+/* ---------------------------------- SETUP --------------------------------- */
+void setup() 
 {
-        // put your setup code here, to run once:
-        Serial.begin(115200); 
+        Serial.begin(115200);
+        Serial.setTimeout(250); 
         pinMode(13, OUTPUT); 
-        digitalWrite(13, LOW);
-        Serial.setTimeout(100000);
-        //initialize the buffer
-        for (byte i = 0; i < 8; i++) {
-                buf[i] = '\0'; 
-        }
+
 }
+/* -------------------------------------------------------------------------- */
 
-
-
+/* ---------------------------------- LOOP ---------------------------------- */
 void loop()
 {
-        // put your main code here, to run repeatedly:
-        /* ------------------------------ test ------------------------------ */
-        delay(100);
-        if (received == false){
-                Serial.println(F("READY"));
-        } else {
-                Serial.println(buf);
+        // Sample every 250ms and recompute temperatures
+        if ((millis() - LAST_SAMPLE_MS) > 250){
+                BUF_INDEX = (NUM_SAMPLES % 4);
+                TMP_BUF[BUF_INDEX] = TMP.readTempC();
+                TMP_UPPER_BUF[BUF_INDEX] = TMP_UPPER.readTempC();
+                TMP_LOWER_BUF[BUF_INDEX] = TMP_LOWER.readTempC(); 
+                LAST_SAMPLE_MS = millis(); 
+                NUM_SAMPLES++; 
+                TMP_C = sum(TMP_BUF)/4.0;
+                TMP_UPPER_C = sum(TMP_UPPER_BUF)/4.0;
+                TMP_LOWER_C = sum(TMP_LOWER_BUF)/4.0;
         }
-        
-        if (Serial.available() > 0) {
-                Serial.readBytesUntil('\n', buf, 8); 
-                if (strcmp(buf, "ACK") == 0) {
-                        digitalWrite(13, HIGH); 
-                }
-                received++; 
+
+        if ((millis() - test_MS) > 250){
+                Serial.print(F("TMP: "));
+                Serial.print(TMP_C);
+                Serial.print(F(" TMP_UPPER: "));
+                Serial.print(TMP_UPPER_C);
+                Serial.print(F(" TMP_LOWER: "));
+                Serial.println(TMP_LOWER_C);
+                test_MS = millis(); 
         }
-        
-        /* ------------------------------------------------------------------ */
 }
+/* -------------------------------------------------------------------------- */
+
+/* ----------------------------------- SUM ---------------------------------- */
+float sum(const float array[4])
+{
+        float total = 0; 
+        for (short i = 0; i < 4; i++) 
+        {
+                total += array[i]; 
+        }
+        return total; 
+}
+/* -------------------------------------------------------------------------- */
