be9459bbc34a87a6effd1a02747127335c00ecb3
Seth <ses3wn@umsystem.edu>
2023-07-08 22:23:54 -0500

    test animation working

--- python/graphing.py
+++ python/graphing.py
@@ -22,23 +22,40 @@ def runGraph():
         ax.set_ylim(y_range)
         ax.set_xlim(x_range)
         
-        # create setpoint curve
+        # create setpoint line
         xs_sp = []
         ys_sp = []
         for k in SETPOINT_LIST:
                 xs_sp.append(k[0])
                 ys_sp.append(k[1])
         setpoint_line, = ax.plot(xs_sp, ys_sp, color='xkcd:purple', linestyle=':')
-        setpoint_line.set_label('Target')
 
-        #legend
-        ax.legend(loc='upper left')
+        #create temp line
+        tmp_line, = ax.plot(XS_TMP, YS_TMP, color='xkcd:red', linestyle='solid')
 
         # labels
         plt.title('REFLOW TEMPERATURE')
         plt.xlabel('Time (s)')
         plt.ylabel('Temp (C)')
+        setpoint_line.set_label('Target')
+        ax.legend(loc='upper left')
+
+        # This function updates temp line with new data and is called by FuncAnimation()
+        def animate(i):
+                XS_TMP.append(0.5*i)
+                YS_TMP.append(0.5*i)
+                tmp_line.set_xdata(XS_TMP)
+                tmp_line.set_ydata(YS_TMP)
+                return tmp_line,
+
+        # Setup FuncAnimation
+        ani = animation.FuncAnimation(fig, animate, interval=10, blit=True,
+                                        cache_frame_data=False)
+                                
         plt.show()
+
+        return
+
 
 
 # ---------------------------------------------------------------------------- #
